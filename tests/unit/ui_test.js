11252d0ee252582515cc2df82e7e1947d0e00dba
Phil Crosby <phil.crosby@gmail.com>
2022-06-25 22:41:04 -0700

    Add a test for buildKeyMappingsPrefixes

--- tests/unit/ui_test.js
+++ tests/unit/ui_test.js
@@ -1,5 +1,7 @@
 import * as shoulda from "../vendor/shoulda.js";
 import {context, should, assert} from "../vendor/shoulda.js";
+import "../../content_scripts/sheet_actions.js";
+import "../../content_scripts/commands.js";
 import "../../content_scripts/ui.js";
 
 context("UI", () => {
@@ -7,5 +9,19 @@ context("UI", () => {
     const o = {a: "b", c: "d"};
     assert.equal({b: "a", d: "c"},
                  invertObjectMap(o));
+  });
+
+  should("buildKeyMappingsPrefixes", () => {
+    const mappings = {
+      "normal": {
+        "moveUp": "a•b•c",
+        "moveDown": "a•d",
+        "moveLeft": "x•y",
+        "moveDown": "z"
+      }
+    };
+    const result = UI.buildKeyMappingsPrefixes(mappings);
+    assert.equal({ "a": true, "a•b": true, "x": true},
+                 result["normal"]);
   });
 });
